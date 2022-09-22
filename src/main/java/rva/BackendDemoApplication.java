4167c2036878dee4945b965a5320f556b45d6714
AnaMarijaKarcas <anamarijakarcas00@gmail.com>
2022-09-22 21:54:28 +0200

    component scan

--- src/main/java/rva/BackendDemoApplication.java
+++ src/main/java/rva/BackendDemoApplication.java
@@ -2,7 +2,9 @@ package rva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages= {"rva.ctrls"})
 @SpringBootApplication
 public class BackendDemoApplication {
 
