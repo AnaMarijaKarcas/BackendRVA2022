db49e902ffdb43906cb93147fa966d66d501ef3f
AnaMarijaKarcas <anamarijakarcas00@gmail.com>
2022-09-22 22:04:18 +0200

    component scan three

--- src/main/java/rva/BackendDemoApplication.java
+++ src/main/java/rva/BackendDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"rva.ctrls"})
+@ComponentScan(basePackages= {"rva.ctrls", "rva.jpa", "rva.repositories"})
 public class BackendDemoApplication {
 
 	public static void main(String[] args) {
